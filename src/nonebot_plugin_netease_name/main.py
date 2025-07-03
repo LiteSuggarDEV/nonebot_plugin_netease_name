6ee744ab65995e6d8a81973e5a76f8dfe46986a7
John Richard <windowserror@163.com>
2025-07-03 15:55:47 +0800

    修改文件读取模式

--- src/nonebot_plugin_netease_name/main.py
+++ src/nonebot_plugin_netease_name/main.py
@@ -14,15 +14,22 @@ from pathlib import Path
 
 from nonebot_plugin_localstore import get_plugin_config_dir
 
+NAME_STRU_DICT = {
+    "内建1": "前缀+人名+动词",
+    "内建2": "前缀+人物+动词",
+    "内建3": "前缀+人物+动词",
+    "内建4": "前缀+形容+人名",
+    "内建5": "前缀+动词+人物",
+    "内建6": "前缀+动词+人名",
+    "内建7": "人名+#的+前缀+物品",
+}
 application_start_time = time.time()
 config_dir = get_plugin_config_dir()
 # 获取目录
-path = __file__
-path = path[: len(path) - 7]
-cwd = Path(path)
+path = Path(__file__).parent
 
 # 处理db
-dbfs = os.listdir(cwd / "db")
+dbfs = os.listdir(path / "db")
 dbs = {}
 f2s = {}
 try:
@@ -33,19 +40,20 @@ except:  # noqa: E722
 
 # 写入变量
 for filename in dbfs:
-    with open(f"{path}db/{filename}") as file:
+    with open(path / "db" / filename) as file:
         dbs[filename] = file.read().split("\n")
 
-with open(f"{path}db/字到音") as file:
+with open(path / "db" / "字到音") as file:
     lines = file.read().split("\n")
     for item in lines:
         lineitems = item.split(",")
         f2s[lineitems[0]] = lineitems[1]
-if not (config_dir/"name_strus.json").is_file() or not (config_dir/"name_strus.json").exists():
-    with open(f"{path}name_strus.json") as file:
-        name_stru: dict[str, str] = json.load(file)
+if (
+    not (config_dir / "name_strus.json").is_file()
+    or not (config_dir / "name_strus.json").exists()
+):
     with open(config_dir/"name_strus.json", "w") as f:
-        json.dump(name_stru, f,indent=4)
+        json.dump(NAME_STRU_DICT, f, indent=4)
 else:
     with open(config_dir/"name_strus.json") as file:
         name_stru: dict[str, str] = json.load(file)
@@ -57,7 +65,6 @@ name_stru_keys = list(name_stru.keys())
 def init_random():
     seed = (hashlib.sha512(str(time.time()).encode()).hexdigest())[:8]
     seed = int(seed, 16)
-    ##print(f"初始化到种子{seed}")
     random.seed(seed)
 
 
